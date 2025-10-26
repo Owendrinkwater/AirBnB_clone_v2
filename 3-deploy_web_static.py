f444e87902847bfa5dd61088cc2a1e13ddfc01bb
Owendrinkwater <owenowade@gmail.com>
2025-10-26 22:09:09 +0300

    fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -18,7 +18,7 @@ def do_pack():
             file_name = "versions/web_static_{}.tgz".format(date)
             local("tar -cvzf {} web_static".format(file_name))
             return file_name
-        except Exception:
+    except Exception:
             return None
 
 
